bdd70e3bff6bd016b411f73360e67bc12e2d4f86
navsingh3211 <navsingh3211@gmail.com>
2024-08-29 14:28:04 +0530

    react.createElement

---
+++ inception-1/app.js
@@ -0,0 +1,42 @@
+/**
+ * 
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>I am h1 tag</h1>
+ *          <h2>I am h2 tag</h2>
+ *      </div>
+ * </div>
+ * 
+ * 
+ * ==> React.createElement(Create an Object),not a html
+ * 
+ */
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
+    )
+);// type of parent is object
+console.log(parent);
+
+
+
+
+// const heading = React.createElement(
+//     "h1", 
+//     { id: "heading", xyz: "abc" }, 
+//     "Hello world from react!"
+// ); //{} is used to pass attribute inside h1 tag
+
+// console.log(heading);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+root.render(parent)
