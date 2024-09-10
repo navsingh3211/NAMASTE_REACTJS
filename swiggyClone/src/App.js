6fc23d45add5a70e03ec4e096e7e1962b93c59aa
navsingh3211 <navsingh3211@gmail.com>
2024-09-10 20:40:40 +0530

    router

--- swiggyClone/src/App.js
+++ swiggyClone/src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 /**
  * Header
@@ -29,5 +33,21 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement:<Error/>
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ swiggyClone/src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = ()=>{
+    return (
+        <div>
+            <h1>COntact Us</h1>
+            <h2>contact us page</h2>
+        </div>
+    )
+}
+
+export default Contact;
