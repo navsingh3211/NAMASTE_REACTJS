a877038cb4584aa0c99208e935c8d5cb580c493e
navsingh3211 <navsingh3211@gmail.com>
2024-09-21 02:21:10 +0530

    testing unit basic

--- tailwindSwiggy/src/components/__tests__/Contact.test.js
+++ tailwindSwiggy/src/components/__tests__/Contact.test.js
@@ -2,11 +2,49 @@ import {render,screen} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Contact from "../Contact"
 
-test("testing for contact us",()=>{
+test("should load contact us component",()=>{
     render(<Contact/>); //we are trying to render contact page on js-dom
     // screen.debug();
     const heading = screen.getByRole("heading");
 
     // Assertion
     expect(heading).toBeInTheDocument()
-})
+});
+
+test("should load button inside contact us component",()=>{
+    render(<Contact/>); //we are trying to render contact page on js-dom
+    // screen.debug();
+    const button = screen.getByRole("button");
+
+    // Assertion
+    expect(button).toBeInTheDocument()
+});
+
+test("should sumbit text inside contact us component",()=>{
+    render(<Contact/>); //we are trying to render contact page on js-dom
+    // screen.debug();
+    const button = screen.getByText("Submit");
+
+    // Assertion
+    expect(button).toBeInTheDocument()
+});
+
+test("should find placeholder inputName inside contact us component",()=>{
+    render(<Contact/>); //we are trying to render contact page on js-dom
+    // screen.debug();
+    const inputName = screen.getByPlaceholderText("name");
+
+    // Assertion
+    expect(inputName).toBeInTheDocument();
+});
+
+test("Should load 2 input boxes on the Contact component",()=>{
+    render(<Contact/>);
+
+    //Querying 
+    const inputBox = screen.getAllByRole("textbox"); //this will jsx element which will be an array
+    // console.log(inputBox,'inputBox');
+    
+    //Assertion
+    expect(inputBox.length).toBe(2);
+});
