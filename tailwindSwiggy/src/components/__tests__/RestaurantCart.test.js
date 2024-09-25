e7c13e3a239490de3375ee2f58819e80ce071e03
navsingh3211 <navsingh3211@gmail.com>
2024-09-25 13:10:32 +0530

    test

--- tailwindSwiggy/src/components/__tests__/RestaurantCart.test.js
+++ tailwindSwiggy/src/components/__tests__/RestaurantCart.test.js
@@ -1,12 +1,12 @@
 import RestaurantCard from "../RestaurantCard";
 import { render, screen } from "@testing-library/react";
 import restroList from "../MockData/mockdataArr";
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom"; //it provide browser like environment
 
 test("should render resturantCard component with props data", () => {
   render(<RestaurantCard resData={restroList} />);
 
   const cardData = screen.getByText("Azad Hind Dhaba");
-//   screen.debug()
+  //screen.debug()
   expect(cardData).toBeInTheDocument();
 });
